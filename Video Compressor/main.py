f52df624c7ac87f45e23cd25013bf2e8b2498a25
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-01-27 15:54:52 +0400

    bare minimum

--- Video Compressor/main.py
+++ Video Compressor/main.py
@@ -1,14 +1,41 @@
 import os
 from numpy import round
+import pathlib
 import subprocess
 
-print(os.chdir("Python-Scripts/Video Compressor"))
+print(os.getcwd())
 
 def size_mb(path):
     return f"{round(os.path.getsize(path) / 1024 / 1024, 2)} MB"
 
+def compress(path):
+    basename = pathlib.Path(path).stem
+    extension = pathlib.Path(path).suffix
+    output_filepath = f"videos/{basename}_compressed{extension}"
+    
+    print(basename, extension, output_filepath)
+
+    subprocess.run([
+        "ffmpeg",
+        "-i", path,
+        "-c:v", "libx264",
+        "-crf", "28", # Classic is 23, but I needed stronger compression.
+        "-preset", "slow",
+        "-c:a", "copy",
+        output_filepath
+    ], check=True)
+
+    return output_filepath
+
+def main():
+    pass
 
 if __name__ == "__main__":
+    main()
+    
     filepath = "videos/Шифрование.mp4"
     size_before = size_mb(path=filepath)
+    output_filepath = compress(path=filepath)
+    size_after = size_mb(path=output_filepath)
     print(size_before)
+    print(size_after)
