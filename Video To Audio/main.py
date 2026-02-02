8d04a9e46691a1609694a13451f94757ad281ca9
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-02-02 12:17:22 +0400

    Video to audio. function

--- Video To Audio/main.py
+++ Video To Audio/main.py
@@ -1,12 +1,25 @@
 import subprocess
+from pathlib import Path
 
-subprocess.run([
-    "ffmpeg",
-    "-i", "videos/video.mp4",
-    "-vn",
-    "-ac", "1",
-    "-ar", "16000",
-    "video.wav"
-], check=True)
+BASE_DIR = Path("output_audio")
 
+def video_to_audio(filepath: Path):
+    if not filepath.exists():
+        raise FileNotFoundError(f"File not found: {filepath}")
+
+    filename = filepath.stem
+    file_extension = filepath.suffix
+
+    output_path = BASE_DIR / f"{filename}.wav"
+
+    subprocess.run([
+        "ffmpeg",
+        "-i", filepath,
+        "-vn",
+        "-ac", "1",
+        "-ar", "16000",
+        output_path
+    ], check=True)
+
+video_to_audio(filepath=Path("input_video/video.mp4"))
 
